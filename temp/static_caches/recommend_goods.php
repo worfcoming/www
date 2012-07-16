da40f428ba80aa2dadd027e9f62311f3600e8562
joomlajoy <1755516099@qq.com>
2012-07-16 11:27:20 +0800

    first
    2012-7-16

---
+++ temp/static_caches/recommend_goods.php
@@ -0,0 +1,48 @@
+<?php
+$data = array (
+  'best' => 
+  array (
+    0 => 
+    array (
+      'goods_id' => '1',
+      'sort_order' => '100',
+    ),
+    1 => 
+    array (
+      'goods_id' => '2',
+      'sort_order' => '100',
+    ),
+    2 => 
+    array (
+      'goods_id' => '3',
+      'sort_order' => '100',
+    ),
+  ),
+  'new' => 
+  array (
+  ),
+  'hot' => 
+  array (
+    0 => 
+    array (
+      'goods_id' => '1',
+      'sort_order' => '100',
+    ),
+    1 => 
+    array (
+      'goods_id' => '2',
+      'sort_order' => '100',
+    ),
+    2 => 
+    array (
+      'goods_id' => '3',
+      'sort_order' => '100',
+    ),
+  ),
+  'brand' => 
+  array (
+    1 => '保时捷',
+    3 => '保时捷',
+  ),
+);
+?>
