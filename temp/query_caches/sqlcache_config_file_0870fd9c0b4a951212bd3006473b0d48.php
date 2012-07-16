340f930ca71442909ce3aa3329592035bbf5b4a4
joomlajoy <1755516099@qq.com>
2012-07-16 18:06:30 +0800

    oo

--- temp/query_caches/sqlcache_config_file_0870fd9c0b4a951212bd3006473b0d48.php
+++ temp/query_caches/sqlcache_config_file_0870fd9c0b4a951212bd3006473b0d48.php
@@ -1,5 +1,5 @@
 <?php
-$this->mysql_config_cache_file_time = 1342432036;
+$this->mysql_config_cache_file_time = 1342433116;
 $this->timeline = 0;
 $this->timezone = 28800;
 $this->platform = 'WINDOWS';
